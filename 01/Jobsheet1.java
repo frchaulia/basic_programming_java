bc6841d3202f3df0f3c99438d86a987d333ad5cf
frchaulia <149360908+frchaulia@users.noreply.github.com>
2023-11-01 22:31:31 +0700

    Add files via upload

---
+++ 01/Jobsheet1.java
@@ -0,0 +1,12 @@
+public class Jobsheet1 {
+    public static void main(String[] args) {
+        System.out.println("=====================================");
+        System.out.println("        New Student Biography        ");
+        System.out.println("=====================================");
+        System.out.println("Name        : Faricha Aulia");
+        System.out.println("NIM         : 2141720155");
+        System.out.println("Departement : Information Technology");
+        System.out.println("Address     : Malang");
+        System.out.println("=====================================");
+    }
+}
